bf4be4f2b7f2a427ebd1057d6ea3514eb4d7a34a
MaxV <maxsdtgv@gmail.com>
2019-05-13 23:48:56 +0300

    function defining

--- awschecker.cpp
+++ awschecker.cpp
@@ -6,25 +6,53 @@
 #include <stdlib.h> 
 #include <string.h> 
 #include <arpa/inet.h>
+#include <netdb.h>
 
-#define PORT 2000
+uint dns_to_ip(char *dns_in);
 
+uint dns_to_ip(char *dns_in){
+    struct hostent *hp = gethostbyname(dns_in);
+    char aaa111[INET_ADDRSTRLEN];
+    if (hp == NULL) {
+       printf("gethostbyname() failed\n");
+    } else {
+       printf("%s = ", hp->h_name);
 
-int main()
+       inet_ntop(AF_INET, hp -> h_addr_list[0], aaa111, INET_ADDRSTRLEN);            
+
+       printf( "%s \n", aaa111);
+    }	
+	printf("%s >>>> !!!\n",aaa111);
+	return *aaa111;
+}
+
+int main(int argc, char **argv)
 {
   	
-	struct sockaddr_in address;
+
+    if (argc < 2) {
+        printf("Usage: %s hostname", argv[0]);
+        exit(-1);
+    }
+
+    //char aaaddd[INET_ADDRSTRLEN];
+    //aaaddd = *dns_to_ip(argv[1]);
+	//printf("%s --- \n", aaaddd);
+  	dns_to_ip(argv[1]);
+  	//printf("%s >>>", aaaddd);
+
+
 	struct sockaddr_in serv_addr;
 
-	char *hello = "Hello44\n";
+	char *hello = "HelloMy";
 	char buffer[1024] = {0};
 
-	int sock = 0, valread;
+	int sock = 0;
 
 	memset(&serv_addr, '0', sizeof(serv_addr));
 
 	serv_addr.sin_family = AF_INET; 
-    serv_addr.sin_port = htons(PORT); 
+    serv_addr.sin_port = htons(atoi(argv[2])); 
 
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
     { 
@@ -33,7 +61,7 @@ int main()
     } 
 
     // Convert IPv4 and IPv6 addresses from text to binary form 
-    if(inet_pton(AF_INET, "192.168.1.168", &serv_addr.sin_addr)<=0)  
+    if(inet_pton(AF_INET, "1.1.1.1", &serv_addr.sin_addr)<=0)  
     { 
         printf("\nInvalid address/ Address not supported \n"); 
         return -1; 
@@ -46,7 +74,7 @@ int main()
     } 
     send(sock , hello , strlen(hello) , 0 ); 
     printf("Hello message sent\n"); 
-    valread = read( sock , buffer, 1024); 
+    read( sock , buffer, 1024); 
     printf("%s\n",buffer ); 
 
 
